33e65416b6adb7047c7b32b7a07f2f0c97357247
ihoment_liyilong <yilong.li@ihoment.com>
2021-04-08 20:09:52 +0800

    提交新代码（V1.0.2版）

--- src/main/java/com/lee/ClassWebsiteMain.java
+++ src/main/java/com/lee/ClassWebsiteMain.java
@@ -3,6 +3,9 @@ package com.lee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author lee
+ */
 @SpringBootApplication
 public class ClassWebsiteMain {
     public static void main(String[] args) {
